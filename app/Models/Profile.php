745997addb71bddd2b8727a0dd161505d062633b
Sabard Pierre <pierresabard@gmail.com>
2025-03-03 15:32:37 +0100

    feat(profile): add first version for profile model

---
+++ app/Models/Profile.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+
+/**
+ * @property int $id
+ * @property string $lastName
+ * @property string $firstName
+ * @property string $image
+ * @property \Carbon\Traits\Timestamp $created_at
+ * @property \Carbon\Traits\Timestamp $updated_at
+ */
+class Profile extends Model
+{
+
+    protected $table = "profile";
+
+    protected $connection = "mysql";
+
+    protected $primaryKey = "id";
+
+
+    protected $fillable = [
+        'lastname',
+        'firstname',
+        'image',
+        'status',
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Get profile first name
+     */
+    protected function firstName(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => ucfirst($value),
+        );
+    }
+
+
+    /**
+     * Get profile last name
+     */
+    protected function lastName(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => ucfirst($value),
+        );
+    }
+}
