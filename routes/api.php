08f582850f12bf5816d654a7d138d62c346eedd7
Sabard Pierre <pierresabard@gmail.com>
2025-03-04 17:42:02 +0100

    refacto route to be under "api" names

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
 Route::controller(ProfileController::class)
     ->prefix('/profiles')
     ->group(
