7e752819a0fd6c9d42107328b3792807e0b87ae0
Sabard Pierre <pierresabard@gmail.com>
2025-03-04 22:50:28 +0100

    Revert "Merge branch 'master' into feat/auth"
    This reverts commit 859dde1ab5e1d1025e17c649e936764daf9871cb, reversing
    changes made to 87141bd9a904bd00f92365ab9b524e3c76b6da21.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,12 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        then: function () {
-            Route::middleware(['api', ForceJsonResponse::class])
-                ->group(base_path('routes/profile.php'));
-        }
     )
-    ->withMiddleware(function (Middleware $middleware) {})
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
