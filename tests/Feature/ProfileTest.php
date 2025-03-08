f078c41e70ccabd3d4811585a5bc3b1c41fa22c8
Sabard Pierre <pierresabard@gmail.com>
2025-03-08 21:53:39 +0100

    test(profile): test get a profile

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -44,3 +44,42 @@ test('get all profiles authenticated', closure: function () {
         });
     });
 });
+
+test('get a profile unauthenticated', closure: function () {
+    /** @var Profile $testProfile */
+    $testProfile = Profile::factory()->create();
+
+    $response = $this->get('/api/profiles/'.$testProfile->id);
+    $response->assertStatus(200);
+    $response->assertJsonIsObject(key: 'data');
+
+    $response->assertJson(function (AssertableJson $json) {
+        $json->has(key: 'data');
+        $json->whereAllType([
+            'data.id' => 'integer',
+            'data.last_name' => 'string',
+            'data.first_name' => 'string',
+            'data.image' => 'string',
+        ]);
+        $json->missing('status');
+    });
+});
+test('get a profile authenticated', closure: function () {
+    /** @var Profile $testProfile */
+    $testProfile = Profile::factory()->create();
+    Passport::actingAs(User::factory()->create());
+    $response = $this->get('/api/profiles/'.$testProfile->id);
+    $response->assertStatus(200);
+    $response->assertJsonIsObject(key: 'data');
+
+    $response->assertJson(function (AssertableJson $json) {
+        $json->has(key: 'data');
+        $json->whereAllType([
+            'data.id' => 'integer',
+            'data.last_name' => 'string',
+            'data.first_name' => 'string',
+            'data.image' => 'string',
+            'data.status' => 'string',
+        ]);
+    });
+});
