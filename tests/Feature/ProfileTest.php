d08814070e6e09a67c0e4fb90da3dc7b7a843ec4
Sabard Pierre <pierresabard@gmail.com>
2025-03-10 00:52:03 +0100

    test(profile): test profile creation

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Profile;
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Laravel\Passport\Passport;
 
@@ -43,7 +44,6 @@ test('get all profiles authenticated', closure: function () {
         });
     });
 });
-
 test('get a profile unauthenticated', closure: function () {
     /** @var Profile $testProfile */
     $testProfile = Profile::factory()->create();
@@ -71,6 +71,40 @@ test('get a profile authenticated', closure: function () {
     $response->assertStatus(200);
     $response->assertJsonIsObject(key: 'data');
 
+    $response->assertJson(function (AssertableJson $json) {
+        $json->has(key: 'data');
+        $json->whereAllType([
+            'data.id' => 'integer',
+            'data.last_name' => 'string',
+            'data.first_name' => 'string',
+            'data.image' => 'string',
+            'data.status' => 'string',
+        ]);
+    });
+});
+test('create a profile unauthenticated', closure: function () {
+
+    $testProfile = Profile::factory()->make();
+    $response = $this->post('/api/profiles', [
+        $testProfile->toArray(),
+    ]);
+
+    $response->assertStatus(401);
+
+});
+test('create a profile authenticated', closure: function () {
+
+    /** @var Profile $testProfile */
+    $testProfile = Profile::factory()->make();
+    Passport::actingAs(User::factory()->make());
+    $response = $this->post('/api/profiles', [
+        'firstName' => $testProfile->firstName,
+        'lastName' => $testProfile->lastName,
+        'image' => UploadedFile::fake()->image($testProfile->image),
+
+    ]);
+
+    $response->assertStatus(201);
     $response->assertJson(function (AssertableJson $json) {
         $json->has(key: 'data');
         $json->whereAllType([
