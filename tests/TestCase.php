1023b121f6e147af6ab46ae0ef56eb715c7c47df
Sabard Pierre <pierresabard@gmail.com>
2025-03-10 15:45:26 +0100

    test(setup): add setup to have a personnal access client for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,29 @@
 
 namespace Tests;
 
+use DateTime;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
+use Laravel\Passport\ClientRepository;
 
 abstract class TestCase extends BaseTestCase
 {
     //
 
     protected $seed = true;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $clientRepository = new ClientRepository;
+        $client = $clientRepository->createPersonalAccessClient(
+            null, 'Test Personal Access Client', 'http://localhost'
+        );
+
+        DB::table('oauth_personal_access_clients')->insert([
+            'client_id' => $client->id,
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime,
+        ]);
+    }
 }
