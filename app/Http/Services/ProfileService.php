87c038aa2607d596ac35268599bae99490d47f8c
Sabard Pierre <pierresabard@gmail.com>
2025-03-10 14:04:20 +0100

    fix(service): add constant for storage disk

--- app/Http/Services/ProfileService.php
+++ app/Http/Services/ProfileService.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ProfileService
 {
+    const STORAGE_DISK = 'public_images';
+
     /**
      * Stores images for profile
      *
@@ -17,7 +19,7 @@ class ProfileService
     {
         $imageFolder = 'user_'.$profileId;
         try {
-            $storageLink = Storage::disk('public_images')->put($imageFolder, $image);
+            $storageLink = Storage::disk(ProfileService::STORAGE_DISK)->put($imageFolder, $image);
             $imagePath = asset('images/'.$storageLink);
         } catch (Exception $exception) {
             throw new FileException($exception->getMessage(), $exception->getCode(), $exception);
